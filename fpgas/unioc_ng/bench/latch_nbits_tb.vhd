b7e2d6a4679c63ec0c8c30104b52cc8a1146cdb4
lamygalle <lamygalle@gmail.com>
2009-01-31 15:23:27 +0000

    ajout du latch 32 bits

---
+++ fpgas/unioc_ng/bench/latch_nbits_tb.vhd
@@ -0,0 +1,145 @@
+-------------------------------------------------------------------------------
+-- Title      : test bench du fichier latch_nbits_tb.vhd
+-- Project    : 
+-------------------------------------------------------------------------------
+-- File       : latch_nbits_tb.vhd
+-- Author     :   <Paul@PITHIVIER>
+-- Company    : 
+-- Created    : 2008-08-06
+-- Last update: 2008-08-07
+-- Platform   : 
+-- Standard   : VHDL'87
+-------------------------------------------------------------------------------
+-- Description: 
+-------------------------------------------------------------------------------
+-- Copyright (c) 2008 
+-------------------------------------------------------------------------------
+-- Revisions  :
+-- Date        Version  Author  Description
+-- 2008-08-06  1.0      Paul	Created
+-------------------------------------------------------------------------------
+
+
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+USE ieee.numeric_std.ALL;
+USE work.fction_spi.ALL;
+
+ENTITY latch_nbits_tb IS
+  GENERIC(
+    CONSTANT c_periode_maj_data : IN time := 200 ns;
+    CONSTANT c_periode_clk : IN time := 37 ns;
+		CONSTANT bus_width_c : natural RANGE 0 TO 127 := 32
+    );
+  PORT (
+    deltax_o         : OUT std_logic_vector(bus_width_c-1 DOWNTO 0);  -- data to be latched
+    deltay_o         : OUT std_logic_vector(bus_width_c-1 DOWNTO 0);  -- data to be latched
+    squal_o          : OUT std_logic_vector(bus_width_c-1 DOWNTO 0);  -- data to be latched
+    deltax_latched_o : OUT std_logic_vector(bus_width_c-1 DOWNTO 0);  -- data latched
+    deltay_latched_o : OUT std_logic_vector(bus_width_c-1 DOWNTO 0);  -- data latched
+    squal_latched_o  : OUT std_logic_vector(bus_width_c-1 DOWNTO 0)  -- data latched
+    );
+END latch_nbits_tb;
+
+ARCHITECTURE robotter OF latch_nbits_tb IS
+
+
+  COMPONENT latch_nbits IS
+  
+  GENERIC (
+    CONSTANT bus_width_c : natural RANGE 0 TO 127 := 32);  -- width of the bus latched
+
+  PORT (
+    clk_i            : IN  std_ulogic;
+    reset_ni         : IN  std_ulogic;
+    deltax_i         : IN  std_logic_vector(bus_width_c-1 DOWNTO 0);  -- data to be latched
+    deltay_i         : IN  std_logic_vector(bus_width_c-1 DOWNTO 0);  -- data to be latched
+    squal_i          : IN  std_logic_vector(bus_width_c-1 DOWNTO 0);  -- data to be latched
+    deltax_latched_o : OUT std_logic_vector(bus_width_c-1 DOWNTO 0);  -- data latched
+    deltay_latched_o : OUT std_logic_vector(bus_width_c-1 DOWNTO 0);  -- data latched
+    squal_latched_o  : OUT std_logic_vector(bus_width_c-1 DOWNTO 0);  -- data latched
+    latch_data_i     : IN  std_ulogic);  -- latches squal_i detlay_i deltax_i while it is active (i.e. '1')
+END COMPONENT;
+
+
+  
+
+  SIGNAL clk_s : std_ulogic := '0';
+  SIGNAL reset_ns: std_ulogic;
+  SIGNAL deltax_s: std_logic_vector(bus_width_c-1 DOWNTO 0) := (OTHERS => '0');  -- data to be latched
+  SIGNAL deltay_s: std_logic_vector(bus_width_c-1 DOWNTO 0) := (OTHERS => '0');  -- data to be latched
+  SIGNAL squal_s : std_logic_vector(bus_width_c-1 DOWNTO 0) := (OTHERS => '0');  -- data to be latched
+	SIGNAL latch_data_s : std_ulogic := '0';
+  
+BEGIN  -- robotter
+
+  entity_testee : latch_nbits
+	GENERIC MAP(
+    bus_width_c => bus_width_c)  -- width of the bus latched
+
+  PORT MAP(
+    clk_i    =>clk_s,
+    reset_ni => reset_ns,
+    deltax_i => deltax_s,
+    deltay_i => deltay_s,
+    squal_i  => squal_s,
+    deltax_latched_o => deltax_latched_o,
+    deltay_latched_o => deltay_latched_o,
+    squal_latched_o  => squal_latched_o,
+    latch_data_i => latch_data_s);  -- latches data_i while it is active (i.e. '1')
+
+
+
+  horloge: PROCESS
+  BEGIN  -- PROCESS horloge
+
+    clk_s <= not(clk_s);
+    WAIT FOR c_periode_clk/2;
+  END PROCESS horloge;
+
+  reset : PROCESS
+  BEGIN  -- PROCESS
+    reset_ns <= '1';
+    WAIT FOR 6 us;
+    reset_ns <= '0';
+    WAIT FOR 1 us;
+    reset_ns <= '1';
+    WAIT FOR 1 ms;
+    reset_ns <= '0';
+    WAIT FOR 1 us;
+    reset_ns <= '1';
+  END PROCESS;
+
+  latch_data : PROCESS
+  BEGIN  -- PROCESS
+		latch_data_s <= '0';
+    WAIT FOR 1.67 us;
+    latch_data_s <= '1';
+		WAIT FOR 2us;
+  END PROCESS;
+
+
+  -- purpose: envoi des donnees sur le bus spi
+  -- type   : sequential
+  -- inputs : 
+  -- outputs: 
+  maj_data: PROCESS 
+  BEGIN  -- PROCESS envoi_data
+    WAIT FOR c_periode_maj_data;
+		
+		deltax_s <= std_logic_vector(unsigned(deltax_s) + 1);
+		deltay_s <= std_logic_vector(unsigned(deltay_s) + 1);
+		squal_s <= std_logic_vector(unsigned(squal_s) + 1);
+		
+  END PROCESS maj_data;
+  
+	
+	deltax_o <= deltax_s;
+  deltay_o <= deltay_s;
+  squal_o <= squal_s;
+  
+  
+END robotter;
+
+
+
---
+++ fpgas/unioc_ng/src/latch_nbits.vhd
@@ -0,0 +1,110 @@
+-----------------------------------------------------------------------------
+-- Title    : ADNS 6010 Control Unit
+-- Project  : UNIOC_NG Optic Encoders
+-----------------------------------------------------------------------------
+-- File     : adns6010_controlunit.vhd
+-- Author   : JD (jd@robotter.fr)
+-- Company  : Rob'Otter
+-- 
+-- Creation date : 24/01/2009
+-- Platform : Altera Cyclone
+-----------------------------------------------------------------------------
+-- Description : Control unit for the automated mode of optical sensors.
+-- This entity shall :
+-- * access sequentialy each ADNS / using SPI MOTION BURST.
+-- * store and output sum of delta_x, delta_y for each ADNS
+-- * output immediate value of squal for each ADNS
+-----------------------------------------------------------------------------
+-- This program is free software; you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation; either version 2, or (at your option)
+-- any later version.
+-- 
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+-- 
+-- You should have received a copy of the GNU General Public License
+-- along with this program; if not, write to the Free Software
+-----------------------------------------------------------------------------
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+ENTITY latch_nbits IS
+  
+  GENERIC (
+    bus_width_c : natural RANGE 0 TO 127 := 32);  -- width of the bus latched
+
+  PORT (
+    clk_i            : IN  std_ulogic;
+    reset_ni         : IN  std_ulogic;
+    deltax_i         : IN  std_logic_vector(bus_width_c-1 DOWNTO 0);  -- data to be latched
+    deltay_i         : IN  std_logic_vector(bus_width_c-1 DOWNTO 0);  -- data to be latched
+    squal_i          : IN  std_logic_vector(bus_width_c-1 DOWNTO 0);  -- data to be latched
+    deltax_latched_o : OUT std_logic_vector(bus_width_c-1 DOWNTO 0);  -- data latched
+    deltay_latched_o : OUT std_logic_vector(bus_width_c-1 DOWNTO 0);  -- data latched
+    squal_latched_o  : OUT std_logic_vector(bus_width_c-1 DOWNTO 0);  -- data latched
+    latch_data_i     : IN  std_ulogic);  -- latches data_i while it is active (i.e. '1')
+END latch_nbits;
+
+ARCHITECTURE latch_n_bits_1 OF latch_nbits IS
+
+  BEGIN  -- latch_n_bits_1
+    
+    latch_deltax: PROCESS (clk_i)
+        VARIABLE last_latch_data_v : std_ulogic;  -- memory of the last sample of latch_data_i
+    BEGIN  -- PROCESS latch
+      IF rising_edge(clk_i) THEN  -- rising clock edge   
+
+          IF reset_ni = '0' THEN
+          last_latch_data_v := latch_data_i;
+          
+        ELSIF latch_data_i = '1' AND last_latch_data_v = '0' THEN
+          deltax_latched_o <= deltax_i;
+        END IF;
+
+        last_latch_data_v := latch_data_i;
+            
+      END IF;
+    END PROCESS latch;
+
+
+    latch_deltay: PROCESS (clk_i)
+        VARIABLE last_latch_data_v : std_ulogic;  -- memory of the last sample of latch_data_i
+    BEGIN  -- PROCESS latch
+      IF rising_edge(clk_i) THEN  -- rising clock edge   
+          IF reset_ni = '0' THEN
+          last_latch_data_v := latch_data_i;
+          
+        ELSIF latch_data_i = '1' AND last_latch_data_v = '0' THEN
+          deltay_latched_o <= deltay_i;
+        END IF;
+
+        last_latch_data_v := latch_data_i;
+            
+      END IF;
+    END PROCESS latch;
+
+
+
+    latch_squal: PROCESS (clk_i)
+        VARIABLE last_latch_data_v : std_ulogic;  -- memory of the last sample of latch_data_i
+    BEGIN  -- PROCESS latch
+      IF rising_edge(clk_i) THEN  -- rising clock edge   
+        IF reset_ni = '0' THEN
+          last_latch_data_v := latch_data_i;
+          
+        ELSIF latch_data_i = '1' AND last_latch_data_v = '0' THEN
+          squal_latched_o <=  squal_i;
+        END IF;
+
+        last_latch_data_v := latch_data_i;
+            
+      END IF;
+    END PROCESS latch;
+
+
+  END latch_n_bits_1;
