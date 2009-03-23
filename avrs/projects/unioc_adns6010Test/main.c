c19b24f0707cdbe6bc3423d886e7c1e9ff6dc0c4
Jean Damien Brossillon <jeandamien.brossillon@gmail.com>
2009-03-23 23:13:02 +0000

    first position test

--- avrs/projects/unioc_adns6010Test/main.c
+++ avrs/projects/unioc_adns6010Test/main.c
@@ -6,7 +6,6 @@
 #include <math.h>
 
 #include <adns6010.h>
-#include <adns6010_spi.h>
 
 int main(void)
 {
@@ -115,6 +114,32 @@ int main(void)
 
   printf("t | x1 | y1 | s1 | x2 | y2 | s2 | x3 | y3 | s3 | fault\n");
 
+  double Mx[6] = {
+    - 0.1597928  ,
+    - 0.0375097  ,
+    - 0.0871382  ,
+    0.0688487  ,
+    - 0.0368006  ,
+    - 0.0411320 };
+  double My[6] = {
+    - 0.1723755  ,
+    - 0.0298261  ,
+    - 0.0677853  ,
+    0.0772286  ,
+    - 0.0175276  ,
+    - 0.0566467};
+  double Ma[6] = {
+    - 0.1694723  ,
+    - 0.0335015  ,
+    - 0.0929089  ,
+    0.0684750  ,
+    - 0.0387185  ,
+    - 0.0482066  };
+
+  double lx1 = 0,ly1 = 0,lx2 = 0,ly2 = 0,lx3 = 0,ly3 = 0;
+  double vx1,vy1,vx2,vy2,vx3,vy3;
+  double dx,dy,da;
+  double x = 0, y = 0, a = 0;
   uint32_t t=0;
   while(1)
   {
@@ -122,14 +147,53 @@ int main(void)
 
     adns6010_encoders_get_value(&adnsenc);
     
-    printf("%ld %6.6ld %6.6ld %d %6.6ld %6.6ld %d %6.6ld %6.6ld %d %d\n",
-              t,
-              adnsenc.x1, adnsenc.y1, adnsenc.squal1,
-              adnsenc.x2, adnsenc.y2, adnsenc.squal2,
-              adnsenc.x3, adnsenc.y3, adnsenc.squal3,
-              adnsenc.fault);
+    vx1 = adnsenc.x1 - lx1;
+    vy1 = adnsenc.y1 - ly1;
+    vx2 = adnsenc.x2 - lx2;
+    vy2 = adnsenc.y2 - ly2;
+    vx3 = adnsenc.x3 - lx3;
+    vy3 = adnsenc.y3 - ly3;
 
-    wait_ms(20);
+    lx1 = adnsenc.x1;
+    ly1 = adnsenc.y1;
+    lx2 = adnsenc.x2;
+    ly2 = adnsenc.y2;
+    lx3 = adnsenc.x3;
+    ly3 = adnsenc.y3;
+/*
+    printf("%6.6ld %6.6ld %6.6ld %6.6ld %6.6ld %6.6ld | ",
+              adnsenc.x1, adnsenc.y1,
+              adnsenc.x2, adnsenc.y2,
+              adnsenc.x3, adnsenc.y3);
+    */
+    dx = Mx[0] * vx1 
+      + Mx[1] * vy1
+      + Mx[2] * vx2
+      + Mx[3] * vy2
+      + Mx[4] * vx3
+      + Mx[5] * vy3;
+
+    dy = My[0] * vx1 
+      + My[1] * vy1
+      + My[2] * vx2
+      + My[3] * vy2
+      + My[4] * vx3
+      + My[5] * vy3;
+
+    da = Ma[0] * vx1 
+      + Ma[1] * vy1
+      + Ma[2] * vx2
+      + Ma[3] * vy2
+      + Ma[4] * vx3
+      + Ma[5] * vy3;
+
+    x += dx;
+    y += dy;
+    a += da;
+    
+    if(!(t%10))
+      printf(" %2.2f %2.2f %2.2f\n",x,y,a);
+ //     printf(" x = %2.2f y = %2.2f a = %2.2f\n",x,y,a);
   }
 
   return 0;
