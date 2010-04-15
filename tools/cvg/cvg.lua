abd5aba7cd8ff84da474e3800ac3d19d0671f8e5
Benoît Ryder <benoit@ryder.fr>
2010-04-15 16:26:32 +0000

    Reflected I2C protocol changes and new orders.

--- tools/cvg/cvg.lua
+++ tools/cvg/cvg.lua
@@ -4,31 +4,39 @@ local M = {}
 
 
 -- Recv and send methods which will be used.
-local cb_send, cb_recv
+local cb_send  -- cb_send(addr, data)
+local cb_recv  -- cb_recv(addr, size) -> string
+-- Sleep method
+local cb_sleep -- cb_sleep(usec)
 
 
 -- Slave metatable, define slave methods.
 local slave_mt = {
 
-  -- Find buffer data with given id
-  get_data = function(self, id)
-    for k,v in ipairs(self.buffer) do
-      if v.name == id then
+  -- Find order with given id or name
+  get_order = function(self, id)
+    for k,v in ipairs(self.orders) do
+      if v.id == id or v.name == id then
         do return v end
       end
     end
     return nil
   end,
 
+  -- Send raw order payload.
+  send_raw = function(self, o, data)
+    assert(#data == o.args.size, "invalid params size: expected "..tostring(o.args.size).." got "..tostring(#data))
+    cb_send(self.roid, string.char(o.id, #data)..data)
+  end,
+
   -- Send data.
-  -- params may be a string or a table of byte values (raw data), or a table of
-  -- named (typed) values
+  --   id: order id or name
+  --   params: string or table of byte values (raw data), or table of named
+  --           (typed) values
   send = function(self, id, params)
 
-    d = self:get_data(id)
-    assert(d ~= nil, "id not found: "..tostring(id))
-
-    assert(d.mode ~= 'R', "invalid access: "..id.." is not writable")
+    local o = self:get_order(id)
+    assert(o ~= nil, "order not found: "..tostring(id))
 
     local sout  -- output string
     if type(params) == 'string' then
@@ -39,7 +47,7 @@ local slave_mt = {
       if params[1] == nil then
         -- named values
         sout = ''
-        for pk,pv in ipairs(d.params) do
+        for pk,pv in ipairs(o.args) do
           local x = params[pv.name]
           if type(x) == 'string' then
             -- string
@@ -67,36 +75,39 @@ local slave_mt = {
       error("invalid params type")
     end
 
-    assert(#sout == d.size, "invalid data size: expected "..tostring(d.size).." got "..tostring(#sout))
-    cb_send(self.roid, d.offset, sout)
+    self:send_raw(o, sout)
 
   end,
 
-  -- Receive raw data as a string.
-  recv_raw = function(self, id)
+  -- Receive raw order response as a string.
+  recv_raw = function(self, o)
 
-    d = self:get_data(id)
-    assert(d ~= nil, "id not found: "..tostring(id))
-
-    assert(d.mode ~= 'W', "invalid access: "..id.." is not readable")
-
-    return cb_recv(self.roid, d.offset, d.size)
+    local data = cb_recv(self.roid, o.ret.size+1)
+    if data.byte(1) == 0 then
+      return nil -- no payload
+    end
+    assert( #data == o.ret.size+1, "invalid data size")
+    local ret = data:sub(2)
+    assert( data:byte(1) == o.ret.size, "invalid payload size")
+    return ret
 
   end,
 
   -- Receive data as a table of data.
+  -- Return nil if data is not ready.
   recv = function(self, id)
 
-    d = self:get_data(id)
-    assert(d ~= nil, "id not found: "..tostring(id))
+    local o = self:get_order(id)
+    assert(o ~= nil, "order not found: "..tostring(id))
 
-    assert(d.mode ~= 'W', "invalid access: "..id.." is not readable")
-
-    sdata = cb_recv(self.roid, d.offset, d.size)
+    local sdata = self:recv_raw(o)
+    if sdata == nil then
+      return nil
+    end
 
     local t = {}
     local i = 1
-    for pk,pv in ipairs(d.params) do
+    for pk,pv in ipairs(o.ret) do
       s = sdata:sub(i,i+pv.size-1)
       if pv.fmt == 's' then
         t[pv.name] = s
@@ -110,7 +121,7 @@ local slave_mt = {
           m = m * 256
         end
         -- transform to negative value if needed
-        if pv.fmt == 'd' and x > m/2 then
+        if pv.fmt == 'o' and x > m/2 then
           x = x - m
         end
         t[pv.name] = x
@@ -120,6 +131,29 @@ local slave_mt = {
 
     return t
 
+  end,
+
+
+  -- Send data and poll until a response is received or timeout expires.
+  -- Return data as a table or nil on timeout.
+  call = function(self, id, params, polldt, tout)
+    self:send(id, params)
+    local ttot = 0
+    while true do
+      local ret = self:recv(id)
+      if ret ~= nil then break end
+      if ttot >= tout then
+        return nil
+      end
+      if ttot + polldt then
+        cb_sleep( tout-ttot )
+        ttot = tout
+      else
+        cb_sleep( polldt )
+        ttot = ttot + polldt
+      end
+    end
+    return ret
   end
 
 }
@@ -129,19 +163,20 @@ slave_mt.__index = slave_mt
 -- Create a slave from a table.
 -- Check data and fill out some fields.
 --
--- t should have the following format:
+-- t should have the following format (some fields are optional)
 --   t = {
 --     roid = 42,
---     buffer = {
---       { name = 'MESSAGE_NAME', mode = <MODE>,  params = {
---         {name='pname', size=2, fmt=<FMT>},
---         ...
---       } },
+--     orders = {
+--       { id=0x12, name = 'MESSAGE_NAME', args = <PARAMS>, ret = <PARAMS> },
 --       ...
 --     }
 --   }
+-- 
+-- with <PARAMS> = {
+--   { name = 'param_name', size = byte_size, fmt = <FMT>
+--   ...
+-- }
 --
--- <MODE> is 'R', 'W', or 'RW'
 -- <FMT> is 'u' (unsigned), 'd' (signed), 'b' (boolean) or 's' (string)
 --
 function M.slave_init(t)
@@ -150,56 +185,58 @@ function M.slave_init(t)
   assert(t.roid, "invalid 'roid' value")
   t.roid = math.floor(t.roid)
   assert(t.roid > 0 and t.roid < 256, "invalid 'roid' value")
-  assert(type(t.buffer) == 'table', "missing 'buffer' field")
+  assert(type(t.orders) == 'table', "missing 'orders' field")
 
-  local offset = 2  -- sub-addresses start at 2
-  for k,v in ipairs(t.buffer) do
-    assert(type(v) == 'table', "invalid 'buffer' element")
+  local t_ids = {}  -- to check multiple use of ids/names
+  local t_names = {}
+  for k,v in ipairs(t.orders) do
+    assert(type(v) == 'table', "invalid 'orders' element")
     -- checks fields
+    v.id = math.floor(v.id)
+    assert(type(v.id) == 'number', "invalid 'id' field")
     assert(type(v.name) == 'string', "invalid 'name' field")
-    assert(type(v.params) == 'table', "invalid 'params' field")
-    assert(#v.params > 0, "empty 'params' field")
-    s = v.mode
-    assert(s=='R' or s=='W' or s=='RW', "unexpected 'mode' value: "..tostring(s))
+    assert( not t_ids[v.id], "order ID already used")
+    assert( not t_names[v.name], "order name already used")
+    t_ids[v.id] = true
+    t_names[v.name] = true
+    if v.args == nil then v.args = {} end
+    if v.ret == nil then v.ret = {} end
+    assert(type(v.args) == 'table', "invalid 'args' field")
+    assert(type(v.ret) == 'table', "invalid 'ret' field")
 
-    -- offset
-    if v.offset == nil then
-      v.offset = offset
-    else
-      v.offset = tonumber(v.offset)
-      assert(v.offset == offset, "offset mismatch, expected "..tostring(offset).." got "..tostring(v.offset))
-    end
-
-    -- params
-    local poffset, size
-    size = 0
-    for pk,pv in ipairs(v.params) do
-      assert(type(pv) == 'table', "invalid 'params' element")
-      -- checks fields
-      assert(type(pv.name) == 'string', "invalid 'name' field")
-      pv.size = math.floor(pv.size)
-      assert(pv.size > 0, "invalid 'size' value: "..tostring(pv.size))
-      s = pv.fmt
-      assert(s=='u' or s=='d' or s=='b' or s=='s', "unexpected 'fmt' value: "..tostring(s))
-      if s == 'b' then assert(pv.size == 1, "size must be 1 for boolean values") end
-      size = size + pv.size
-    end
-
-    -- size
-    assert(size < 256-2, "buffer size is too big: "..tostring(size))
-    if v.size == nil then
-      v.size = size
-    else
-      v.size = tonumber(v.size)
-      assert(v.size == size, "size mismatch, expected "..tostring(size).." got "..tostring(v.size))
-    end
-
-    offset = offset + size
-
+    -- check parameters, compute size
+    M.check_params(v.args)
+    M.check_params(v.ret)
   end
 
   return setmetatable(t, slave_mt)
 
+end
+
+-- Compute a checksum of given data.
+function M.checksum(data)
+  local sum = 0
+  for i=1,#data do sum = sum + data:byte(i) end
+  return sum % 256
+end
+
+-- Check a parameter table, set its size.
+function M.check_params(t)
+  local poffset = 0
+  for pk,pv in ipairs(t) do
+    assert(type(t) == 'table', "invalid parameter element")
+    -- checks fields
+    assert(type(pv.name) == 'string', "invalid 'name' field")
+    assert(type(pv.size) == 'number', "invalid 'size' field")
+    pv.size = math.floor(pv.size)
+    assert(pv.size > 0, "invalid 'size' value: "..tostring(pv.size))
+    s = pv.fmt
+    assert(s=='u' or s=='d' or s=='b' or s=='s', "unexpected 'fmt' value: "..tostring(s))
+    if s == 'b' then assert(pv.size == 1, "size must be 1 for boolean values") end
+    pv.offset = poffset
+    poffset = poffset + pv.size
+  end
+  t.size = poffset
 end
 
 
@@ -209,13 +246,24 @@ local cb_default = function()
 end
 cb_recv = cb_default
 cb_send = cb_default
+cb_sleep = function(usec)
+  os.execute("usleep "..tonumber(usec))
+end
 
 -- Register recv/send methods
-function M.set_callbacks(f_recv, f_send)
-  assert(type(f_recv) == 'function', "invalid recv callback")
-  assert(type(f_send) == 'function', "invalid send callback")
-  cb_recv = f_recv
-  cb_send = f_send
+function M.set_callbacks(f_recv, f_send, f_sleep)
+  if f_recv ~= nil then
+    assert(type(f_recv) == 'function', "invalid recv callback")
+    cb_recv = f_recv
+  end
+  if f_send ~= nil then
+    assert(type(f_send) == 'function', "invalid send callback")
+    cb_send = f_send
+  end
+  if f_sleep ~= nil then
+    assert(type(f_sleep) == 'function', "invalid sleep callback")
+    cb_sleep = f_sleep
+  end
 end
 
 
--- tools/cvg/cvg_example.lua
+++ tools/cvg/cvg_example.lua
@@ -5,22 +5,25 @@
 require('cvg')
 
 
-function i2c_send(addr, offset, data)
+function i2c_send(addr, data)
   local s = ''
   for i=1,#data do
     s = s .. string.format(" %02X", data:byte(i))
   end
-  print(string.format("SEND @ %02X+%02X : %s", addr, offset, s))
+  print(string.format("SEND @ %02X : %s", addr, s))
 end
-function i2c_recv(addr, offset, size)
+function i2c_recv(addr, size)
   local data = ''
-  local s = ''
-  for i=1,size do
+  for i=1,size-1 do
     local n = (i*0x42) % 256
-    data = data .. string.char( n )
-    s = s .. string.format(" %02X", n)
+    data = data .. string.char(n)
   end
-  print(string.format("RECV @ %02X+%02X : %s", addr, offset, s))
+  data = string.char(#data)..data
+  local s = ''
+  for i=1,#data do
+    s = s .. string.format(" %02X", data:byte(i))
+  end
+  print(string.format("RECV @ %02X : %s", addr, s))
   return data
 end
 
@@ -29,42 +32,84 @@ cvg.set_callbacks(i2c_recv, i2c_send)
 
 -- slave description
 slave = {
-  roid = 0x42,
-  buffer = {
-    { name = 'POSITION_TARGET_A_XYA', mode = 'W',  params = {
+  roid = 0x20,
+  name = 'UNIOC-NG',
+  orders = {
+    { id=0x00, name = 'NONE' },
+    { id=0x01, name = 'FORTYTWO', ret = {
+      {name='v', size=1, fmt='u'},
+    } },
+    { id=0x10, name = 'GOTO_A_XYA', args = {
       {name='x', size=2, fmt='d'},
       {name='y', size=2, fmt='d'},
       {name='a', size=2, fmt='d'}
     } },
-    { name = 'POSITION_TARGET_R_XYA', mode = 'W',  params = {
+    { id=0x11, name = 'GOTO_R_XYA', args = {
       {name='x', size=2, fmt='d'},
       {name='y', size=2, fmt='d'},
       {name='a', size=2, fmt='d'}
     } },
-    { name = 'POSITION_CURRENT_XYA',  mode = 'RW', params = {
+    { id=0x20, name = 'TRAJECTORY_SET_CHECKPOINT', args = {
+      {name='i', size=1, fmt='u'},
+      {name='x', size=2, fmt='d'},
+      {name='y', size=2, fmt='d'}
+    } },
+    { id=0x21, name = 'TRAJECTORY_SET_RUN', args = {
+      {name='n', size=1, fmt='u'},
+    } },
+    { id=0x22, name = 'TRAJECTORY_STATUS', ret = {
+      {name='b', size=1, fmt='u'},
+    } },
+    { id=0x23, name = 'TRAJECTORY_AUTOSET', args = {
+      {name='n', size=1, fmt='u'},
+    } },
+    { id=0x30, name = 'SET_A_SPEED', args = {
+      {name='v', size=2, fmt='u'},
+      {name='a', size=2, fmt='u'}
+    } },
+    { id=0x31, name = 'SET_XY_CRUISE_SPEED', args = {
+      {name='v', size=2, fmt='u'},
+      {name='a', size=2, fmt='u'}
+    } },
+    { id=0x32, name = 'SET_XY_STEERING_SPEED', args = {
+      {name='v', size=2, fmt='u'},
+      {name='a', size=2, fmt='u'}
+    } },
+    { id=0x33, name = 'SET_XY_STOP_SPEED', args = {
+      {name='v', size=2, fmt='u'},
+      {name='a', size=2, fmt='u'}
+    } },
+    { id=0x34, name = 'SET_STEERING_WIN', args = {
+      {name='r', size=2, fmt='u'},
+    } },
+    { id=0x35, name = 'SET_STOP_WIN', args = {
+      {name='r', size=2, fmt='u'},
+      {name='l', size=2, fmt='u'},
+    } },
+    { id=0x40, name = 'GET_XYA', ret = {
       {name='x', size=2, fmt='d'},
       {name='y', size=2, fmt='d'},
       {name='a', size=2, fmt='d'}
     } },
-    { name = 'SPEED_XY',              mode = 'W',  params = {
-      {name='speed', size=2, fmt='d'},
-      {name='acc',   size=2, fmt='d'}
+    { id=0x41, name = 'SET_XYA', args = {
+      {name='x', size=2, fmt='d'},
+      {name='y', size=2, fmt='d'},
+      {name='a', size=2, fmt='d'}
     } },
-    { name = 'SPEED_A',               mode = 'W',  params = {
-      {name='speed', size=2, fmt='d'},
-      {name='acc',   size=2, fmt='d'}
+    { id=0x50, name = 'SET_BRAKE', ret = {
+      {name='b', size=1, fmt='b'},
     } },
-    { name = 'BRAKE',                 mode = 'W',  params = {
-      {name='brake', size=1, fmt='b'}
+    { id=0x51, name = 'GET_ADNSFAULT', ret = {
+      {name='fault', size=1, fmt='u'},
     } },
-    { name = 'AUTOSET',               mode = 'RW', params = {
-      {name='border', size=1, fmt='u'}
+    { id=0x52, name = 'GET_ADNSSQUALS', ret = {
+      {name='squal1', size=1, fmt='u'},
+      {name='squal2', size=1, fmt='u'},
+      {name='squal3', size=1, fmt='u'},
     } },
-    { name = 'COMPASS',               mode = 'W',  params = {
-      {name='active', size=1, fmt='b'}
-    } },
-    { name = 'STATUS',                mode = 'R',  params = {
-      {name='status', size=1, fmt='u'}
+    { id=0x53, name = 'GET_TIME', ret = {
+      {name='sec',  size=4, fmt='u'},
+      {name='usec', size=4, fmt='u'},
     } },
   },
 }
@@ -74,9 +119,9 @@ slave = {
 slave = cvg.slave_init(slave)
 
 -- interact!
-slave:send('SPEED_XY', {0x61,0x62,0x63,42})
-slave:send('POSITION_TARGET_R_XYA', { x=':)', y=0x63, a=-2 })
-t = slave:recv('POSITION_CURRENT_XYA')
+slave:send('SET_A_SPEED', {0x61,0x62,0x63,42})
+slave:send('GOTO_R_XYA', { x=':)', y=0x63, a=-2 })
+t = slave:recv('GET_XYA')
 print(string.format("  x: %d, y: %d, a: %d", t.x, t.y, t.a))
 
 
