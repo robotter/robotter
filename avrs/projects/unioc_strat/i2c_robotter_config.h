21a583eedad525aa696193160d6bd17f79f7ffcd
Benoît Ryder <benoit@ryder.fr>
2011-02-26 17:43:51 +0000

    Cleaning, prepare to use the ppp module.

--- avrs/projects/unioc_strat/i2c_robotter_config.h
+++ avrs/projects/unioc_strat/i2c_robotter_config.h
@@ -1,4 +1,7 @@
 
+/// Byte send when there is nothing to send.
+#define I2C_SEND_NONE_BYTE  0x00
+
 /** @name Slave specific options. */
 //@{
 #ifdef I2C_SLAVE
@@ -7,9 +10,6 @@
 #define I2CS_RECV_BUF_SIZE    8
 /// Send buffer size.
 #define I2CS_SEND_BUF_SIZE    8
-
-/// Byte send when there is nothing to send.
-#define I2CS_SEND_NONE_BYTE  0x00
 
 #endif
 //@}
