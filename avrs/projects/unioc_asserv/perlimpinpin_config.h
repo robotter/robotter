fb2913bade61015acd62d79839cf4c266779cec7
Benoît Ryder <benoit@ryder.fr>
2011-02-26 22:46:04 +0000

    Update ppp config.

--- avrs/projects/unioc_asserv/perlimpinpin_config.h
+++ avrs/projects/unioc_asserv/perlimpinpin_config.h
@@ -9,16 +9,16 @@
  *
  * @note This value should always be defined.
  */
-#define PPP_DEVICE_ROID   ROID_DEVICE_NAME
+#define PPP_DEVICE_ROID   ROID_PROP
 
 /// Enable UART transmissions using given UART number.
-//#define PPP_UART_NUM  0
+#define PPP_UART_NUM  1
 
 /// Enable I2C master mode.
 //#define PPP_I2C_MASTER
 
 /// Enable I2C slave mode.
-//#define PPP_I2C_SLAVE
+#define PPP_I2C_SLAVE
 
 /// Number I2C master attempts to receive reply.
 #define PPP_I2CM_RECV_MAX_TRIES  10
