eeb7b97f90d4ee1a26374522948cdf4d6ee8e01b
Benoît Ryder <benoit@ryder.fr>
2011-05-30 17:58:51 +0200

    PPP: implementation of common orders

---
+++ avrs/modules/comm/perlimpinpin/perlimpinpin_common.c
@@ -0,0 +1,21 @@
+#include <avr/wdt.h>
+#include "perlimpinpin_common.h"
+
+/** @brief Handler common PPP messages.
+ * @return 1 if handled, 0 otherwise
+ */
+int8_t ppp_common_callback(const PPPMsgFrame *msg)
+{
+  switch( msg->mid ) {
+    case PPP_MID_RESET:
+      wdt_enable(WDTO_30MS);
+      for(;;) ;
+    case PPP_MID_PING:
+      PPP_SEND_PING_R(msg->src, msg->ping.v);
+      break;
+    default: // not handled
+      return 0;
+  }
+  return 1;
+}
+
---
+++ avrs/modules/comm/perlimpinpin/perlimpinpin_common.h
@@ -0,0 +1,12 @@
+/** @file
+ * @brief Callback for common messages.
+ */
+
+#ifndef PERLIMPINPIN_COMMON_H
+#define PERLIMPINPIN_COMMON_H
+
+#include "perlimpinpin.h"
+
+int8_t ppp_common_callback(const PPPMsgFrame *);
+
+#endif
