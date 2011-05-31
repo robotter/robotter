77fa6ad66422b93db40551a6145c88f467a7751f
Benoît Ryder <benoit@ryder.fr>
2011-05-31 11:32:06 +0200

    R3D2: cleaning

--- avrs/projects/r3d2/r3d2_config.h
+++ avrs/projects/r3d2/r3d2_config.h
@@ -21,20 +21,20 @@
   */
 #ifndef R3D2_CONFIG_H
 #define R3D2_CONFIG_H
-	
-	// default motor speed (XXX manque min max XXX)
-	#define DEFAULT_MOTOR_SPEED	2200
 
-	// default reflector ratio (_
-	#define DEFAULT_SURFACE_REFLECTION_RATIO 2.0
+// default motor speed (XXX manque min max XXX)
+#define DEFAULT_MOTOR_SPEED 2200
 
-	// default number of motor rotation 
-	#define DEFAULT_ROBOT_DETECTED_TIMEOUT_THRESHOLD 5
+// default reflector ratio (_
+#define DEFAULT_SURFACE_REFLECTION_RATIO 2.0
 
-	// default number of motor rotation lost before detecting motor stop (expressed in r3d2_monitor tick)
-	#define DEFAULT_MOTOR_ROTATING_TIMEOUT_THRESHOLD 3
+// default number of motor rotation
+#define DEFAULT_ROBOT_DETECTED_TIMEOUT_THRESHOLD 5
 
-	// angle offset added to the angle of the detected robot (in degree)
-	#define DEFAULT_ROBOT_DETECTED_ANGLE_OFFSET 80
+// default number of motor rotation lost before detecting motor stop (expressed in r3d2_monitor tick)
+#define DEFAULT_MOTOR_ROTATING_TIMEOUT_THRESHOLD 3
+
+// angle offset added to the angle of the detected robot (in degree)
+#define DEFAULT_ROBOT_DETECTED_ANGLE_OFFSET 80
 
 #endif // R3D2_CONFIG_H
