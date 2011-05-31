1cbd0d707117b21835b6f25bc4c8244bcb0b6616
Jean Damien Brossillon <jeandamien.brossillon@gmail.com>
2011-05-31 12:07:59 +0200

    MECA: arm release function added

--- avrs/projects/meca_2011/ppp.c
+++ avrs/projects/meca_2011/ppp.c
@@ -1,8 +1,10 @@
 #include <perlimpinpin_common.h>
+#include <aversive/error.h>
 
 #include "actuators.h"
 #include "scanner.h"
 #include "sys.h"
+#include "settings.h"
 
 extern actuators_t actuators;
 
@@ -17,15 +19,17 @@ void ppp_msg_callback(PPPMsgFrame *msg)
   uint8_t armid;
   switch( msg->mid ) {
     case PPP_MID_ARM_SET_POS:
+    {
       armid = msg->arm_set_pos.arm;
       switch(msg->arm_set_pos.pos)
       {
         case -1: actuators_arm_lower(&actuators, armid); break;
-        case  0: actuators_arm_mid(&actuators,armid); break;
-        case  1: actuators_arm_raise(&actuators,armid); break;
+        case  0: actuators_arm_mid(&actuators, armid); break;
+        case  1: actuators_arm_raise(&actuators, armid); break;
         default: break;
       }
       return;
+    }
 
     case PPP_MID_ARM_SCAN:
       scanner_source_address = msg->src;
@@ -33,6 +37,26 @@ void ppp_msg_callback(PPPMsgFrame *msg)
       {
         case 0: scanner_state = ST_SCHEDULED_LEFT; break;
         case 1: scanner_state = ST_SCHEDULED_RIGHT; break;
+        default: break;
+      }
+      return;
+
+    case PPP_MID_ARM_RELEASE:
+      switch(msg->arm_release.arm)
+      {
+        case 0:
+          actuators_ax12_setPositionSpeed(&actuators, 
+                                            SETTING_AX12_ID_LEFT_ARM,  
+                                            SETTING_AX12_POS_LARM_RAISED,
+                                            300);
+          break;
+
+        case 1:
+          actuators_ax12_setPositionSpeed(&actuators, 
+                                            SETTING_AX12_ID_RIGHT_ARM,  
+                                            SETTING_AX12_POS_RARM_RAISED,
+                                            300);
+          break;
         default: break;
       }
       return;
