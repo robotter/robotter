60cda78c9db0de55f47140d9712cf9a628efd644
Benoît Ryder <benoit@ryder.fr>
2012-03-25 16:44:21 +0200

    bootloader: allow to get/set baudrate on BasicSerial
    Baudrate can be get and set using the 'baudrate' property attribute.
    This is compatible with pySerial interface.

--- avrs/bootloader/basicserial.py
+++ avrs/bootloader/basicserial.py
@@ -4,15 +4,31 @@ import fcntl
 import struct
 
 
-class BasicSerial:
-  """Basic serial connection implementation.
+class BasicSerial(object):
+  """
+  Basic serial connection implementation.
   
   It is merely compatible with the pySerial implementation.
   """
 
   def __init__(self, port, baudrate=38400, bytesize=8, parity=None, stopbits=1):
     self.fd = os.open(port, os.O_RDWR|os.O_NOCTTY)
+    self._baudrate = baudrate
+    self._bytesize = bytesize
+    self._parity = parity
+    self._stopbits = stopbits
+    self.configure_port()
 
+  @property
+  def baudrate(self):
+    return self._baudrate
+
+  @baudrate.setter
+  def baudrate(self, baudrate):
+    self._baudrate = baudrate
+    self.configure_port()
+
+  def configure_port(self):
     iflag, oflag, cflag, lflag, ispeed, ospeed, cc = termios.tcgetattr(self.fd)
 
     iflag &= ~(termios.INPCK|termios.ISTRIP|termios.INLCR|termios.IGNCR|termios.ICRNL|termios.IXON)
@@ -22,7 +38,7 @@ class BasicSerial:
 
     # baudrate
     try:
-      ispeed = ospeed = getattr(termios, 'B%s' % baudrate)
+      ispeed = ospeed = getattr(termios, 'B%s' % self._baudrate)
     except AttributeError:
       raise ValueError("baudrate not supported")
 
@@ -34,28 +50,28 @@ class BasicSerial:
           6: termios.CS6,
           7: termios.CS7,
           8: termios.CS8,
-          }[bytesize]
+          }[self._bytesize]
     except KeyError:
-      raise ValueError("invalid bytesize: %r" % bytesize)
+      raise ValueError("invalid bytesize: %r" % self._bytesize)
 
     # setup parity
-    if parity in (None, 'N'):
+    if self._parity in (None, 'N'):
       cflag &= ~(termios.PARENB|termios.PARODD)
-    elif parity in (0, 'even', 'E'):
+    elif self._parity in (0, 'even', 'E'):
       cflag &= ~(termios.PARODD)
       cflag |=  (termios.PARENB)
-    elif parity in (1, 'odd', 'O'):
+    elif self._parity in (1, 'odd', 'O'):
       cflag |=  (termios.PARENB|termios.PARODD)
     else:
-      raise ValueError("invalid parity: %r" % parity)
+      raise ValueError("invalid parity: %r" % self._parity)
 
     # stopbits
-    if stopbits == 1:
+    if self._stopbits == 1:
       cflag &= ~(termios.CSTOPB)
-    elif stopbits == 2:
+    elif self._stopbits == 2:
       cflag |=  (termios.CSTOPB)
     else:
-      raise ValueError("invalid stopbits: %r" % stopbits)
+      raise ValueError("invalid stopbits: %r" % self._stopbits)
 
     # no blocking, no timeout
     cc[termios.VMIN] = 1
